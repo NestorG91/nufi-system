acdbc7a2b62fa34630ac96a4ab4f1a7863d6f18c
Nestor Gelvez <ngelvez91@gmail.com>
2026-05-25 15:21:06 -0500

    Intalando y agrefando login JavaFX

---
+++ src/main/java/com/nufi/controller/LoginController.java
@@ -0,0 +1,26 @@
+package com.nufi.controller;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+
+public class LoginController {
+
+    @FXML
+    private TextField txtUsuario;
+
+    @FXML
+    private PasswordField txtPassword;
+
+    @FXML
+    public void iniciarSesion() {
+
+        String usuario = txtUsuario.getText();
+
+        String password = txtPassword.getText();
+
+        System.out.println("Usuario: " + usuario);
+
+        System.out.println("Password: " + password);
+    }
+}
